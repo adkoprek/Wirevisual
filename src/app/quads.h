699725e6c57717ea08d44605be25297b395c82ff
Koprek Adam <koprek_a@hipa-lc8.psi.ch>
2024-06-21 14:55:03 +0200

    Added QUADS

---
+++ src/app/quads.h
@@ -0,0 +1,86 @@
+#include <map>
+#include <string>
+#include <vector>
+
+const std::map<std::string, std::vector<std::string>> QUADS = {
+    {"b869", {
+            "QWA1",  "QWA2",  "QWA3",  "QWA4",  "QWA5",  "QWA6",
+            "QWA7",  "QWA8",  "QWA9",  "QWA10", "QWA11", "QWA12",
+            "QWA13", "QWA14", "QWA15", "QWA16", "QWA17", "QWB1",
+            "QWB2",  "QWB3",  "QWB4",  "QWB5",  "QWB6",  "QWB7",
+            "QWB8",  "WVB1",  "WVB2" 
+        }
+    },
+    {"bce", {
+            "QBB1", "QBB2", "QBB3", "QBB4", "QBB5", "QBB6",
+            "QCA1", "QCA2"
+        }
+    },
+    {"bw2", {
+            "WVB1", "WVB2", "QWA1", "QWA2", "QWA3", "QWA4",
+            "QWA5", "QWA6"
+        }
+    },
+    {"bx1", {
+            "QXA1", "QXA2", "QXA3", "QXA4",  "QXA5",  "QXB6",
+            "QXB7", "QXB8", "QXB9", "QXA10", "QXA11", "QXA12",
+            "QXA13"
+        }
+    },
+    {"bx2", {
+            "QXA1", "QXA2", "QXA3"
+        }
+    },
+    {"ip2", {
+           "QYA1", "QYA2", "QYA3", "QYA4",  "QYA5", "QYA6",
+           "QYA7", "QYA8", "QYA9", "QYA10"
+        }
+    },
+    {"iw2", {
+            "QXA1",  "QXA2",  "QXA3",  "QXA4",  "QXA5",  "QXB6",
+            "QXB7",  "QXB8",  "QXB9",  "QXA10", "QXA11", "QXA12",    
+            "QXA13", "QXA14", "QXA15", "QXA16", "QXA17", "QXA18",
+            "QXA19", "QND1",  "QND2"
+        }
+    },
+    {"pkbhe", {
+            "QHA1",   "QHA2",   "QHA3",   "QHA4",   "QHTC5",  "QHB7",
+            "QHB8",   "QHA9",   "QHA10",  "QHC11",  "QHC12",  "QHTC13",
+            "QHTC14", "QHTC15", "QHTC16", "QHTC17", "QHTC18",
+            "QHG21",  "QHG22"
+        }
+    },
+    {"pksinq", {
+            "QHA1",   "QHA2",   "QHA3",   "QHA4",   "QHTC5",  "QHTC6",  
+            "QHB7",   "QHB8",   "QHA9",   "QHA10",  "QHC11",  "QHC12", 
+            "QHTC13", "QHTC14", "QHTC15", "QHTC16", "QHTC17", "QHTC18", 
+            "QHG18",  "QHG21",  "QHG22",  "QHG23",  "QHG24",  "QHI25",  
+            "QHI26",  "QHI27",  "QHI28",  "QHI29",  "QHJ30",  "QHJ31", 
+            "QHJ32"
+        }
+    },
+    {"pktebhe", {
+            "QHG21", "QHG22"
+        }
+    },
+    {"pktm", {
+           "QHA1", "QHA2", "QHA3", "QHA4",  "QHTC5", "QHTC6", 
+           "QHB7", "QHB8", "QHA9", "QHA10", "QHC11", "QHC12" 
+        }
+    },
+    {"pktmte", {
+            "QHTC13", "QHTC14", "QHTC15", "QHTC16", "QHTC17", "QHTC18"
+        }
+    },
+    {"sinq", {
+            "QHG21", "QHG22", "QHG23", "QHG24", "QHI25", "QHI26",
+            "QHI27", "QHI28", "QHI29", "QHJ30", "QHJ31", "QHJ32"
+        }
+    },
+    {"ucn", {
+            "QBB1", "QBB2", "QBB3", "QBB4", "QBB5", "QBB6",
+            "QBB7"
+        }
+    }
+};
+
--- src/logic/epics_commands.h
+++ src/logic/epics_commands.h
@@ -3,6 +3,7 @@
 #include <string>
 
 
+// Profiles and harps
 const std::string PROFILE_PV_X = ":PROF:2:P";
 const std::string PROFILE_PV_Y = ":PROF:2:I";
 const std::string HARP_PV_Y = ":PROF:2";
@@ -10,3 +11,9 @@ const std::string PROFILE_PV_SIZE = ":PROF:2:P.NELM";
 const std::string PROFILE_PV_STATUS = ":PSTE:1";
 const std::string PV_ACTIVATE_SCAN = ":PCOM:1";
 const std::string START_COMMAND = "START";
+
+// Other data for files
+const std::string QUADS_PV_ACS = ":SOL:1";
+const std::string QUADS_PV_AMPS = ":IST:2";
+const std::string QUADS_PV_FIELD = ":IST:3";
+const std::string RADION_MONITOR_PV_AMP = ":IST:2";
